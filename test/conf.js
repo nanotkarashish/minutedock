51e91653d4a4568d0654e57d5b9d50b0d0b99d7e
Douglas Vaz <douglas1992@gmail.com>
2015-06-19 14:49:51 +0530

    Remove verbose comments

--- test/conf.js
+++ test/conf.js
@@ -1,140 +1,47 @@
-// A reference configuration file.
 exports.config = {
-  // ----- How to setup Selenium -----
-  //
-  // There are three ways to specify how to use Selenium. Specify one of the
-  // following:
-  //
-  // 1. seleniumServerJar - to start Selenium Standalone locally.
-  // 2. seleniumAddress - to connect to a Selenium server which is already
-  //    running.
-  // 3. sauceUser/sauceKey - to use remote Selenium servers via SauceLabs.
-  //
-  // If the chromeOnly option is specified, no Selenium server will be started,
-  // and chromeDriver will be used directly (from the location specified in
-  // chromeDriver)
-
-  // The location of the selenium standalone server .jar file, relative
-  // to the location of this config. If no other method of starting selenium
-  // is found, this will default to
-  // node_modules/protractor/selenium/selenium-server...
   seleniumServerJar: "node_modules/protractor/selenium/selenium-server-standalone-2.41.0.jar",
-  // The port to start the selenium server on, or null if the server should
-  // find its own unused port.
   seleniumPort: 4444,
-  // Chromedriver location is used to help the selenium standalone server
-  // find chromedriver. This will be passed to the selenium jar as
-  // the system property webdriver.chrome.driver. If null, selenium will
-  // attempt to find chromedriver using PATH.
   chromeDriver: 'node_modules/protractor/selenium/chromedriver',
-  // If true, only chromedriver will be started, not a standalone selenium.
-  // Tests for browsers other than chrome will not run.
   chromeOnly: false,
-  // Additional command line options to pass to selenium. For example,
-  // if you need to change the browser timeout, use
-  // seleniumArgs: ['-browserTimeout=60'],
   seleniumArgs: [],
 
-  // The timeout for each script run on the browser. This should be longer
-  // than the maximum time your application needs to stabilize between tasks.
   allScriptsTimeout: 11000,
 
-  // ----- What tests to run -----
-  //
-  // Spec patterns are relative to the location of this config.
   specs: [
     'specs/**/*Spec.js',
   ],
 
-  // Patterns to exclude.
   exclude: [],
 
-  // Alternatively, suites may be used. When run without a commad line parameter,
-  // all suites will run. If run with --suite=smoke, only the patterns matched
-  // by that suite will run.
   suites: {
     single: 'specs/searchSpec.js',
     full: 'specs/**/*Spec.js'
   },
 
-  // ----- Capabilities to be passed to the webdriver instance ----
-  //
-  // For a full list of available capabilities, see
-  // https://code.google.com/p/selenium/wiki/DesiredCapabilities
-  // and
-  // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
- 
-
   capabilities:   {
     'browserName': 'chrome'
   },
 
-  // Cannot use phantomjs until https://github.com/ariya/phantomjs/issues/12160 and 
-  // https://github.com/ariya/phantomjs/issues/12186 are resolved
-  // Uncomment above capability and comment below capabitlity to run chrome browser
-  // capabilities :   {
-  //   'browserName': 'phantomjs',
-  //   'phantomjs.binary.path':'./node_modules/phantomjs/bin/phantomjs'    
-  // },
-
-  // If you would like to run more than one instance of webdriver on the same
-  // tests, use multiCapabilities, which takes an array of capabilities.
-  // If this is specified, capabilities will be ignored.
   multiCapabilities: [],
 
-  // ----- More information for your tests ----
-  //
-  // A base URL for your application under test. Calls to protractor.get()
-  // with relative paths will be prepended with this.
   baseUrl: 'do not use',
 
-  // Selector for the element housing the angular app - this defaults to
-  // body, but is necessary if ng-app is on a descendant of <body>  
   rootElement: 'body',
 
-  // A callback function called once protractor is ready and available, and
-  // before the specs are executed
-  // You can specify a file containing code to run by setting onPrepare to
-  // the filename string.
   onPrepare: "setUp/protractorOnPrepare.js",
-
-  // The params object will be passed directly to the protractor instance,
-  // and can be accessed from your test. It is an arbitrary object and can
-  // contain anything you may need in your test.
-  // This can be changed via the command line as:
-  //   --params.login.user 'Joe'
   params: {
     loginCookie : 's:j:{"passport":{"user":"dd7e10bb-7df3-4f2f-b2c5-8ed1da2d255c"}}.9T315i5t70DTjYLD0DB4tX2ssJI2vaIXkvlb8hvZVSI'
   },
 
-  // ----- The test framework -----
-  //
-  // Jasmine and Cucumber are fully supported as a test and assertion framework.
-  // Mocha has limited beta support. You will need to include your own
-  // assertion framework if working with mocha.
   framework: 'jasmine',
   
-  // ----- Options to be passed to minijasminenode -----
-  //
-  // See the full list at https://github.com/juliemr/minijasminenode
   jasmineNodeOpts: {
-    // onComplete will be called just before the driver quits.
     onComplete: null,
-    // If true, display spec names.
     isVerbose: false,
-    // If true, print colors to the terminal.
     showColors: true,
-    // If true, include stack traces in failures.
     includeStackTrace: true,
-    // Default time to wait in ms before a test fails.
     defaultTimeoutInterval: 30000
   },
-
-  // ----- The cleanup step -----
-  //
-  // A callback function called once the tests have finished running and
-  // the webdriver instance has been shut down. It is passed the exit code
-  // (0 if the tests passed or 1 if not).
   onCleanUp: function() {
     
   }
