9ee118abf722f2457a12078455ee509ccf68a729
Douglas Vaz <douglas1992@gmail.com>
2015-06-22 12:25:22 +0530

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,36 +9,14 @@ var gulp = require('gulp')
 gulp.task('minify-js', function(){
 	rjs({
     baseUrl : 'src/static/js',
+    mainConfigFile: 'src/static/js/main.js',
     out: 'main.min.js',
     name: 'main',
     paths : {
-      'jquery' : '//code.jquery.com/jquery-2.1.1.min',
-      'angular' : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.min',
-      'angular-route' : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-route.min',
-      'angular-storage' : './lib/ngStorage',
-      'bootstrap' : '//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min',
-      'quickdate' : './lib/ng-quick-date'
-    },
-    shim : {
-      'angular-storage' : {
-        'exports' : 'angular-storage',
-        'deps' : ['angular']  
-      },
-      'angular-route' : {
-        'exports' : 'angular-route',
-        'deps' : ['angular']  
-      },
-      'quickdate' : {
-        'exports' : 'quickdate',
-        'deps' : ['angular']  
-      },
-      'angular' : {
-        'exports' : 'angular'
-      },
-      'bootstrap' : {
-        'exports' : 'bootstrap',
-        'deps' : ['jquery']
-      }
+        'angular': "empty:",
+        'angular-route': "empty:",
+        'jquery': "empty:",
+        'bootstrap': "empty:"
     }
   })
   .pipe(uglify())
@@ -55,30 +33,30 @@ gulp.task('minify-css', function() {
 gulp.task('build', ['minify-js', 'minify-css']);
 
 gulp.task('start', function () {
-  nodemon({
-    script: 'src/node/minutedock.js'
-  , ext: 'js html'
-  , env: { 'NODE_ENV': 'development' }
-  }).on('error', function(e) { console.err(e); })
+  return nodemon({
+      script: 'src/node/minutedock.js'
+      , ext: 'js html'
+      , env: { 'NODE_ENV': 'test' }
+    }).on('error', function(e) { console.error(e); });
 });
 
 gulp.task('start-api-stub', function () {
-  nodemon({
-    script: 'test/minutedockStub/stub.js'
-  , ext: 'js'
-  , env: { 'NODE_ENV': 'test' }
-  }).on('error', function(e) { console.err(e); })
+  return nodemon({
+      script: 'test/minutedockStub/stub.js'
+      , ext: 'js'
+      , env: { 'NODE_ENV': 'test' }
+    }).on('error', function(e) { console.error(e); });
 });
 
-gulp.task('test', ['start', 'start-api-stub'], function(){
-  gulp.src(['./test/**/*.js'])
+gulp.task('test', function(){
+  return gulp.src(['./test/**/*.js'])
     .pipe(protractor({
-        'configFile': './test/conf.js',
+        'configFile': 'test/conf.js',
         'args': ['--baseUrl', ''],
         'autoStartStopServer': true,
         'debug': true
     }))
-    .on('error', function(e) { console.err(e); })
+    .on('error', function(e) { throw e; })
 });
 
 gulp.task('default', ['build', 'start']);
