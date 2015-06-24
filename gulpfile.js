6946f5b71499f30cc26a4533ef4cea803928d827
Douglas Vaz <douglas1992@gmail.com>
2015-06-24 12:25:04 +0530

    Add packaging tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,11 @@ var gulp = require('gulp')
   , minifyCss = require('gulp-minify-css')
   , rename = require('gulp-rename')
   , uglify = require('gulp-uglify')
-  , protractor = require('gulp-angular-protractor');
+  , protractor = require('gulp-angular-protractor')
+  , del = require('del')
+  , shell = require('gulp-shell')
+  , gutil = require('gulp-util')
+  , rename = require("gulp-rename");
 
 gulp.task('minify-js', function(){
 	rjs({
@@ -59,5 +63,56 @@ gulp.task('test', ['build', 'start'], function(){
     .on('error', function(e) { console.error(e); process.exit(1); })
     .on('end', function() { console.log("Done."); process.exit(); });
 });
+
+gulp.task('clean', function(){
+  del('dist');
+});
+
+function string_src(filename, string) {
+  var src = require('stream').Readable({ objectMode: true })
+  src._read = function () {
+    this.push(new gutil.File({ cwd: "", base: "", path: filename, contents: new Buffer(string) }))
+    this.push(null)
+  }
+  return src
+}
+
+gulp.task('package-src', function(){
+  var distFiles = [
+    './LICENSE.txt',
+    './src/**/*'];
+
+  return gulp.src(distFiles, { base: './' })
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('package-config', function(){
+  var configFiles = [
+    'config/default.json',
+    'config/production.json'
+  ];
+
+  return gulp.src(configFiles, { base: './' })
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('package-README', function(){
+  var instructions = "1) Install foreverjs \n2) Set NODE_CONFIG with config vars\n3) Run 'npm start'";
+  return string_src('README.txt', instructions)
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('package-dependencies', function(){
+  var npm_pkg = require('./package.json');
+  delete npm_pkg.scripts.test;
+  delete npm_pkg.scripts.postinstall;
+  npm_pkg.scripts.start = 'ENV=production forever start -a -l minutedock.log -o ./logs/out.log -e ./logs/err.log src/node/minutedock.js';
+
+  return string_src('_package.json', JSON.stringify(npm_pkg))
+  .pipe(rename('package.json'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('package', ['package-src', 'package-config', 'package-dependencies', 'package-README'], shell.task(['cd dist && mkdir node_modules && mkdir logs && npm install --production']));
 
 gulp.task('default', ['build', 'start']);
